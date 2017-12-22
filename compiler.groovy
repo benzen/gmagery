a841a2a8babf3d998a37e02c9dc137ab658777ff
Benjamin Dreux <benjamin.dreux@code3.ca>
2017-12-22 17:57:22 -0500

    0306 passed

--- compiler.groovy
+++ compiler.groovy
@@ -1,4 +1,3 @@
-// https://mvnrepository.com/artifact/commons-lang/commons-lang
 @Grapes(
     @Grab(group='commons-lang', module='commons-lang', version='2.6')
 )
@@ -11,90 +10,120 @@ import groovy.util.IndentPrinter
 import org.apache.commons.lang.StringEscapeUtils
 import groovy.json.JsonSlurper
 
-
-def compile(file, data){
-  data = data instanceof String ? new JsonSlurper().parseText(data) : data
-  def writer = new StringWriter()
-  def printer = new IndentPrinter(writer, "    ")
-  def template = new XmlParser().parseText(file)
-  def containsComments = file.contains("<!--")
-  def tag = template."@data-tagname"
-  def children = template.'*'
-  def removeEmptyLines = {s -> s.replaceAll(/\n\s*\n/, "\n")}
-  def paramExp = /\{\{(\w*)}\}/
-  def extractVariables = {s -> (s =~ (paramExp)).collect { it[1]} }
-  def escapeHtml = { StringEscapeUtils.escapeHtml(it) }
-  def compileStrWithData = { str, localData ->
-    str.replaceAll(paramExp, {p ->
-      def paramName = extractVariables(p)[0]
-        def value = localData[paramName]
-        escapeHtml(value)
-    })
-  }
-
-  def buildComponent
-  buildComponent = { mb, child ->
-    if (child instanceof String){
-      def str = compileStrWithData(child, data)
-      mb.mkp.yieldUnescaped(str)
-    } else if (child.'*'[0] instanceof groovy.util.Node){
-
-      mb."${child.name()}"{
-        child.children().collect {child2 ->
-          buildComponent(mb, child2)
+class Compiler{
+  def static  compile(file, data){
+    data = data instanceof String ? new JsonSlurper().parseText(data) : data
+    def writer = new StringWriter()
+    def printer = new IndentPrinter(writer, "    ", true, false)
+    def template = new XmlParser().parseText(file)
+    def containsComments = file.contains("<!--")
+    def tag = template."@data-tagname"
+    def children = template.'*'
+    def removeEmptyLines = {s -> s.replaceAll(/\n\s*\n/, "\n")}
+    def paramExp = /\{\{\s*(\w*[.\w*]*)\s*\}\}/
+    def extractVariables = {s -> (s =~ (paramExp)).collect { it[1]} }
+    def escapeHtml = { StringEscapeUtils.escapeHtml("$it") }
+    def attributesToFilter = ["data-if", "data-unless", "data-each"]
+    def stringifyObject
+    stringifyObject = { result, fetchingLengthOfArray = false ->
+      if(result == null) {
+        ""
+      } else if (result instanceof Map){
+        "[object Object]"
+      } else if (result instanceof List) {
+        if(fetchingLengthOfArray){
+          result.size()
+        } else {
+          result.collect({stringifyObject(it)}).join(',')
         }
+      } else {
+        result
       }
-    } else { //child is a groovy.util.Node but without children
-      def escapedAttributes = child.attributes().collectEntries {
+    }
+    def getProperty = {object, String property ->
+      if(property == null) { return null }
+      def fetchingLengthOfArray = property.endsWith(".length")
+      def cleanedProperty = property.trim().replaceAll(/\.length$/,"")
+      def result = cleanedProperty.tokenize('.').inject(object, {obj, prop -> obj?."$prop" })
+
+      stringifyObject(result, fetchingLengthOfArray)
+    }
+    def alwaysAutoCLosingElements = [ "area",  "base",  "br",  "col",  "command",  "embed",  "hr",  "img",  "input",  "keygen",  "link",  "menuitem",  "meta",  "param",  "source",  "track",  "wbr"]
+
+    def compileStrWithData = { str, localData ->
+      str.replaceAll(paramExp, {p ->
+        def paramName = extractVariables(p)[0]
+        escapeHtml(getProperty(localData, paramName))
+      })
+    }
+
+    def escapeAttributes = { child ->
+      child.attributes().collectEntries {
         [it.key, compileStrWithData(it.value, data)]
       }
-      mb."${child.name()}"(removeEmptyLines(child.text()), escapedAttributes){}
     }
+
+    def buildComponent
+    buildComponent = { mb, child ->
+
+      if (child instanceof String){
+        def str = compileStrWithData(child, data)
+        mb.mkp.yieldUnescaped(str)
+      } else if (child.'*'[0] instanceof groovy.util.Node){
+        def escapedAttributes = escapeAttributes(child)
+
+        def eachAttr =  escapedAttributes?.find({ it.key == "data-each" })
+        // println eachAttr
+        // if(eachAttr){
+        //   def eachAttrValue = eachAttr?.value
+        //   def listPath = eachAttrValue =~ /\w* in (\w*[\.\w*]*)/
+        //   def iterVar = eachAttrValue =~ /(\w*) in \w*[\.\w*]*/
+        //
+        //   println eachAttrValue
+        //   println listPath?.size() ? listPath[0][1] : null
+        //   println listPath?.size() ? iterVar[0][1] : null
+        //
+        // }
+
+        mb."${child.name()}"{
+          child.children().collect {child2 ->
+            buildComponent(mb, child2)
+          }
+        }
+      } else { //child is a groovy.util.Node but without children
+        def escapedAttributes = escapeAttributes(child)
+        // println escapedAttributes
+        def ifAttr = escapedAttributes?.find({ it.key == "data-if" })
+        def ifAttrValue = getProperty(data, ifAttr?.value)
+
+        def unlessAttr = escapedAttributes?.find({ it.key == "data-unless" })
+        def unlessAttrValue = getProperty(data, unlessAttr?.value)
+
+        def filteredAttributes = escapedAttributes.findAll({ !attributesToFilter.contains(it.key)})
+
+        if((ifAttr == null|| ifAttrValue) && (unlessAttr == null || !unlessAttrValue)){
+          if(alwaysAutoCLosingElements.contains(child.name())){
+            mb."${child.name()}"(filteredAttributes){}
+          } else {
+            mb."${child.name()}"(removeEmptyLines(compileStrWithData(child.text(), data)), filteredAttributes){}
+          }
+        }
+
+
+      }
+    }
+
+    def mb = new MarkupBuilder(printer)
+    mb.escapeAttributes = false
+    mb.doubleQuotes = true
+    mb."$tag"('') {
+     children.collect { child ->
+        buildComponent(mb, child)
+     }
   }
 
-  def mb = new MarkupBuilder(printer)
-  mb.escapeAttributes = false
-  mb.doubleQuotes = true
-  mb."$tag"('') {
-   children.collect { child ->
-      buildComponent(mb, child)
-   }
-}
-
- def html = writer.toString().trim()
- containsComments ? "<!DOCTYPE html>\n$html" : html
-}
-def ifThenElse = {pred, ifBranch, elseBranch ->}
-def tests = [
-   "0001-empty-template",
-   "0002-flat-children",
-   "0003-nested-children",
-   "0004-html-comments",
-   "0101-escape-text",
-   "0102-escape-attribute",
-   "0103-existing-nested-property",
-
-]
-.collect {
-  [
-    test: it,
-    template: new File("magery-tests/components/$it/template.html").text.trim(),
-    expected: new File("magery-tests/components/$it/expected.html").text.trim(),
-    data: new File("magery-tests/components/$it/data.json").text.trim()
-  ]
-}
-.each {
-  println "Testing $it.test"
-    def compiledTemplate = compile(it.template, it.data)
-    def expected = it.expected
-
-    // println compiledTemplate
-    // println expected
-
-    assert  compiledTemplate ==  expected
-
+   def html = writer.toString().trim()
+   containsComments ? "<!DOCTYPE html>\n$html" : html
+  }
 
 }
-
-// Hello, &lt;script&gt;alert(&quot;TEST&quot;);&lt;/script&gt;!
-// Hello, &lt;script&gt;alert("TEST");&lt;/script&gt;!
---
+++ test.groovy
@@ -0,0 +1,59 @@
+
+import Compiler
+
+def normalizeXmlString = { str ->
+
+  new XmlSlurper(false, false, true).parseText(str).toString()
+}
+def tests = [
+   "0001-empty-template",
+   "0002-flat-children",
+   "0003-nested-children",
+   "0004-html-comments",
+   "0101-escape-text",
+   "0102-escape-attribute",
+   "0103-existing-nested-property",
+   "0104-missing-nested-property",
+   "0105-underscore-variable-in-attribute",
+   "0106-underscore-variable-in-text",
+   "0107-variable-substitution-text",
+   "0108-variable-substitution-number",
+   "0109-variable-substitution-true",
+   "0110-variable-substitution-false",
+   "0112-variable-substitution-undefined",
+   "0113-variable-substitution-object",
+   "0114-variable-substitution-array",
+   "0115-variable-substitution-length-property",
+   "0116-stringify-array-items",
+   "0201-data-if-true",
+   "0202-data-if-false",
+   "0203-data-if-undefined",
+   "0204-data-if-zero",
+   "0205-data-if-empty-string",
+   "0206-data-if-empty-array",
+   // "0207-underscore-variable-in-data-if",
+   "0301-data-unless-true",
+   "0302-data-unless-false",
+   "0303-data-unless-undefined",
+   "0304-data-unless-zero",
+   "0305-data-unless-empty-string",
+   "0306-data-unless-empty-array",
+   "0401-data-each"
+]
+.collect {
+  [
+    test: it,
+    template: new File("magery-tests/components/$it/template.html").text.trim(),
+    expected: new File("magery-tests/components/$it/expected.html").text.trim(),
+    data: new File("magery-tests/components/$it/data.json").text.trim()
+  ]
+}
+.each {
+    println "Testing $it.test"
+
+    def compiledTemplate =  Compiler.compile(it.template, it.data)
+    def expected = it.expected
+
+    assert  compiledTemplate ==  expected
+
+}
